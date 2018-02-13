7554d277721051cec996398fb3d15600f9536850
shohansazal <shohansazal@gmail.com>
2018-02-14 00:12:31 +0600

    Syntax

---
+++ syntax/Syntax.java
@@ -0,0 +1,12 @@
+package com.shohan.syntax;
+
+/**
+ *
+ * @author SHOHAN
+ */
+public class Syntax {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
