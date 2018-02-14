fe0b1fdac0c868958046383239a43cc6742e1628
shohansazal <shohansazal@gmail.com>
2018-02-14 18:43:13 +0600

    Variale & Data Type

---
+++ variable_datatype/VariableDataType.java
@@ -0,0 +1,19 @@
+package com.shohan.variable_datatype;
+
+/**
+ *
+ * @author SHOHAN
+ */
+public class VariableDataType {
+
+    int data = 50; //instance variable or global/instance/non-static/object variable. it initailized default value using type
+    static String m = "shohan"; //static variable
+
+    public void method() {
+        double d = 10.5; //local variable & Local variable must be initialized
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
