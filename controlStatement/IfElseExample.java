9a27e2ef3290e85b52b206d97532d27ee64d2ba4
shohansazal <shohansazal@gmail.com>
2018-02-15 09:50:09 +0600

    Control Statement

---
+++ controlStatement/IfElseExample.java
@@ -0,0 +1,17 @@
+package com.shohan.controlStatement;
+
+/**
+ *
+ * @author SHOHAN
+ */
+public class IfElseExample {
+
+    public static void main(String[] args) {
+        int number = 13;
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}
