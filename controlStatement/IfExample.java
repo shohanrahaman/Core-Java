4ed6c0c81cfa969ed1e53c0df4aa3ae353047618
shohansazal <shohansazal@gmail.com>
2018-02-15 09:52:32 +0600

    Control Statement

---
+++ controlStatement/IfExample.java
@@ -0,0 +1,15 @@
+package com.shohan.controlStatement;
+
+/**
+ *
+ * @author SHOHAN
+ */
+public class IfExample {
+
+    public static void main(String[] args) {
+        int age = 30;
+        if (age > 30) {
+            System.out.println("Age is greater than 30");
+        }
+    }
+}
---
+++ controlStatement/WhileLoopExample.java
@@ -0,0 +1,17 @@
+package com.shohan.controlStatement;
+
+/**
+ *
+ * @author SHOHAN
+ */
+public class WhileLoopExample {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
